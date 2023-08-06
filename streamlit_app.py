16f3269924c0e13b363956de60fc8fbdbe01155b
RohitSreerama <rohit.sreerama@gmail.com>
2023-08-06 10:58:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('breakfast menu)
+                 streamlit.text('omega 3 & idly)
+                 streamlit.text('natu kodi & dosa')
+                 streamlit.text('mutton shawarma')
